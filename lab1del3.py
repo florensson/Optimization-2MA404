583e6cdaf0fcb86f0cdcb370ceb1280941455cda
florensson <72890914+florensson@users.noreply.github.com>
2023-11-30 19:36:59 +0100

    Add files via upload
    Adding solution for det dual problem and shadow price, exercise 3, i),ii)

--- lab1del3.py
+++ lab1del3.py
@@ -194,10 +194,59 @@ In this exercise we will perform a sensitivity analysis of the LP-problem in Exe
 i) Formulate and solve the dual problem. Verify that the solutions are the same.
 '''
 
+
+# Primal problem coefficients
+# we get the arrays from Exercise 1, i)
+c_primal = c
+A_primal = A
+b_primal = b
+
+# Dual problem coefficients
+c_dual = np.array([3900, 2100, 2200])
+A_dual = -A_primal.T
+b_dual = np.array([-700, -1000])
+
+# Solve primal problem
+result_primal = linprog(c_primal, A_ub=A_primal, b_ub=b_primal)
+x_optimal_primal = result_primal.x
+z_optimal_primal = result_primal.fun
+
+# Solve dual problem
+result_dual = linprog(c_dual, A_ub=A_dual, b_ub=b_dual, method='highs')  # Use method='highs'
+y_optimal_dual = result_dual.x
+w_optimal_dual = result_dual.fun
+
+# Verify that the solutions are close enough
+tolerance = 1e-6
+if np.isclose(z_optimal_primal, w_optimal_dual, rtol=tolerance):
+    print("The optimal solutions of the primal and dual problems are close enough.")
+    print(f"Optimal primal solution (x): {x_optimal_primal}")
+    print(f"Optimal dual solution (y): {y_optimal_dual}")
+else:
+    print("The optimal solutions of the primal and dual problems are not close enough.")
+    print(f"Optimal primal solution (x): {x_optimal_primal}")
+    print(f"Optimal dual solution (y): {y_optimal_dual}")
+
+# Debugging print of the actual optimal values
+print(f"Optimal primal objective value: {z_optimal_primal}")
+print(f"Optimal dual objective value: {w_optimal_dual}")
+
+
 '''
 ii) Compute the shadow price of the various constraints.
 '''
+# Assuming shadow_prices contains the shadow prices obtained from the linear programming solution
+shadow_prices = result_primal.fun
 
+# Find the index of the stage with the highest positive shadow price
+max_positive_index = np.argmax(shadow_prices)
+
+# Find the index of the stage with the most negative shadow price
+min_negative_index = np.argmin(shadow_prices)
+
+# Display recommendations
+print(f"Recommendation: Invest the additional 100 hours in Stage {max_positive_index + 1}.")
+print(f"Avoidance Recommendation: Avoid investing the additional hours in Stage {min_negative_index + 1}.")
 
 '''
 iii) Suppose that you are granted 100 extra working hours in either stage I, II or III. Where would
